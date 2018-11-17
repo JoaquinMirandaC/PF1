c3a6e53a44a6bbf5f42e828eec395705ca2a30ba
0184087 <45013399+0184087@users.noreply.github.com>
2018-11-17 16:18:08 -0600

    QR Api

--- PF1/Form1.cs
+++ PF1/Form1.cs
@@ -1,20 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 using System.Windows.Forms;
 
 namespace PF1
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1 ( )
         {
             InitializeComponent();
+        }
+
+        private void button1_Click ( object sender, EventArgs e )
+        {
+            var client = new WebClient();
+            var url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Eample";
+            var request = HttpWebRequest.Create( url );
+            var response = request.GetResponse();
+            var ad = response.GetResponseStream();
+            Image img = Image.FromStream( ad );
+            pictureBox1.Image = img;
+            pictureBox1.Image.Save( @"C:\Users\Usuario\Downloads\IMAGEJPEG.jpeg" ); //,System.Drawing.Imaging.ImageFormat.Jpeg);
+            MessageBox.Show( img.GetType().ToString() );
+        }
+
+        private void button2_Click ( object sender, EventArgs e )
+        {
+            pictureBox1.Image = Image.FromFile( @"C:\Users\Usuario\Downloads\IMAGEJPEG.jpeg" );
+        }
+
+        private void button3_Click ( object sender, EventArgs e )
+        {
+            pictureBox1.Image = null;
         }
     }
 }
